1e390dfc291c2ca6ceb95624b4377220c1e952ba
Nikolas Lahtinen <nikolas.lahtinen@gmail.com>
2017-09-08 23:14:48 +0300

    enable face detection. add isc license.
    feat: enable face and feature detection
    feat: add image sizes
    chore: add isc license

--- serverless/handler.js
+++ serverless/handler.js
@@ -7,6 +7,13 @@ export function hasher(event, context, callback) {
 
   const sizes = {
     'original': image().buildUrl(),
+    '60x60': image().smartCrop(true).resize(60, 60).buildUrl(),
+    '150x300': image().resize(150, 300).buildUrl(),
+    '300x150': image().resize(300, 150).buildUrl(),
+    '400x150': image().resize(400, 150).buildUrl(),
+    'smart400x150': image().resize(400, 150).buildUrl(),
+    'smart150x300': image().smartCrop(true).resize(150, 300).buildUrl(),
+    'smart300x150': image().smartCrop(true).resize(300, 150).buildUrl(),
     'cropped': image().crop(1080, 200, 2840, 2320).buildUrl(),
     'cropped300x0': image().crop(1080, 200, 2840, 2320).resize(300, 0).buildUrl(),
     '300x0': image().resize(300, 0).buildUrl()
